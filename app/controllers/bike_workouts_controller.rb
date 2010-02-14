113572c9c1d518102af056c1b4bd3e84880a7e43
Eric A. Bonney <eric@vanhlebarsoftware.com>
2010-02-14 13:41:47 -0500

    working on core workouts

--- app/controllers/bike_workouts_controller.rb
+++ app/controllers/bike_workouts_controller.rb
@@ -1,5 +1,21 @@
 class BikeWorkoutsController < ApplicationController
-  def bike_workouts
+  before_filter :require_user
+
+  def index
   end
 
+  def new
+    @bike = BikeWorkout.new
+  end
+
+  def create
+    @bike = BikeWorkout.new
+
+    if @bike.save
+      flash[:notice] = "Your bike workout was saved!"
+      redirect_to workouts_url
+    else
+      render :action => :new
+    end
+  end
 end
--- app/controllers/run_workouts_controller.rb
+++ app/controllers/run_workouts_controller.rb
@@ -1,5 +1,21 @@
 class RunWorkoutsController < ApplicationController
-  def run_workouts
+  before_filter :require_user
+
+  def index
   end
 
+  def new
+    @run = RunWorkout.new
+  end
+
+  def create
+    @run = RunWorkout.new
+
+    if @run.save
+      flash[:notice] = "Your run workout was saved!"
+      redirect_to workouts_url
+    else
+      render :action => :new
+    end
+  end
 end
