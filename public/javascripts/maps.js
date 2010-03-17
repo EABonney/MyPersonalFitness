3b8ea234237ea59fdcc0e03537fab11f9282bbe2
Eric A. Bonney <eric@vanhlebarsoftware.com>
2010-03-17 15:55:50 -0400

    Added the "undo" ability to map creation

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -13,6 +13,8 @@ var AUTH_TOKEN;
 var displayType;
 var routename;
 var routetype;
+var distanceFromPrev;
+var prevTotDist;
 
 function init()
 {
@@ -35,6 +37,11 @@ function init()
 
 function newRoute()
 {
+    // initially set all the map controls to disabled.
+    document.getElementById('undopoint').disabled = true;
+    document.getElementById('clearmap').disabled = true;
+    document.getElementById('autocenter').disabled = true;
+    
     if (GBrowserIsCompatible())
     {
         map = new GMap2(document.getElementById("map"));
@@ -58,45 +65,16 @@ function newRoute()
          }
          );
 
-         // Create the map_marker
-         var icon = new GIcon();
-         icon.image = '/images/map_marker.png';
-         icon.iconsize = new GSize(10, 10);
-         icon.iconAnchor = new GPoint(5, 5);
-
          GEvent.addListener(map, "click", function(overlay, latlng)
          {
              var distanceFromPrev = Number(0);
              var prevTotDist = Number(0);
              distMi = Number(0);
              distKm = Number(0);
-             var marker = new GMarker(latlng, icon);
-
+            // var curmarker = new GMarker(latlng, icon);
              dataPoints.push(latlng);
-
-             // calc the total distance
-             for( var i=0; i<dataPoints.length; i++)
-             {
-                if( i!=0 && dataPoints.length !=1 )
-		{
-                    distanceFromPrev = dataPoints[i-1].distanceFrom(dataPoints[i], 3959).toFixed(2);
-                    prevTotDist = parseFloat(distMi) + parseFloat(distanceFromPrev);
-                    distMi = prevTotDist.toFixed(2);
-                    distKm = parseFloat(distMi) * MILES_TO_KM;
-                    distKm = roundNumber(distKm, 2);
-
-
-                    // Create GPolylineOptions argument as an object literal.
-                    // Note that we don't use a constructor.
-                    var polyline = new GPolyline([
-                      dataPoints[i-1],
-                      dataPoints[i]
-                    ], "#000080", 2, 1);
-                    map.addOverlay(polyline);
- 		}
-             }
-             updateDistanceTable(distMi, distKm);
-             map.addOverlay(marker);
+             //map.addOverlay(curmarker);
+             drawRoute();
          }
          );
     }
@@ -243,6 +221,58 @@ function editRoute()
     }
 }
 
+function drawRoute()
+{
+    // calc the total distance
+    for( var i=0; i<dataPoints.length; i++)
+    {
+       var latlng = dataPoints[i];
+       addMarkerToMap( latlng );
+       
+       if( i!=0 && dataPoints.length !=1 )
+       {
+           distanceFromPrev = dataPoints[i-1].distanceFrom(dataPoints[i], 3959).toFixed(2);
+           prevTotDist = parseFloat(distMi) + parseFloat(distanceFromPrev);
+           distMi = prevTotDist.toFixed(2);
+           distKm = parseFloat(distMi) * MILES_TO_KM;
+           distKm = roundNumber(distKm, 2);
+
+           // Create GPolylineOptions argument as an object literal.
+           // Note that we don't use a constructor.
+           var polyline = new GPolyline([
+             dataPoints[i-1],
+             dataPoints[i]
+           ], "#000080", 2, 1);
+           map.addOverlay(polyline);
+       }
+    }
+    updateDistanceTable(distMi, distKm);
+
+    // See if we need to turn on/off the various map control buttons.
+    if( dataPoints.length > 0 )
+    {
+        document.getElementById('undopoint').disabled = false;
+        document.getElementById('clearmap').disabled = false;
+    }
+    else
+    {
+        document.getElementById('undopoint').disabled = true;
+        document.getElementById('clearmap').disabled = true;
+    }
+}
+
+function addMarkerToMap( latlng )
+{
+    // Create the map_marker
+    var icon = new GIcon();
+    icon.image = '/images/map_marker.png';
+    icon.iconsize = new GSize(10, 10);
+    icon.iconAnchor = new GPoint(5, 5);
+    var marker = new GMarker(latlng, icon);
+
+    map.addOverlay(marker);
+}
+
 function updateDistanceTable(distMi, distKm)
 {
     var tdMiles;
@@ -284,7 +314,6 @@ function updateNameType()
         routeType_select.disabled = true;
     }
 }
-
 
 function setAddress(user_address)
 {
@@ -358,6 +387,31 @@ function saveRoute()
         }); // end of the Ajax request
 
     //alert('jsonRoute: ' + jsonRoute);
+}
+
+function undoLastPoint()
+{
+    // First pop off the last point added to the dataPoints array.
+    if( dataPoints.length > 0 )
+    {
+        dataPoints.pop();
+        // re-draw the poly-lines and calc the distance
+        distMi = Number(0);
+        distKm = Number(0);
+        map.clearOverlays();
+        drawRoute();
+    }
+}
+
+function clearMap()
+{
+    dataPoints.length = 0;
+    map.clearOverlays();
+    distMi = 0;
+    distKm = 0;
+    updateDistanceTable(distMi, distKm);
+    document.getElementById('undopoint').disabled = true;
+    document.getElementById('clearmap').disabled = true;
 }
 
 function loadPoints( routePoints )
